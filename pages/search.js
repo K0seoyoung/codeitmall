dc1d23608be11f8d171a40d83601f2d497bba0d0
K0seoyoung <koseo12@hufs.ac.kr>
2025-04-03 04:10:14 +0900

    API 연동하기 첫 시작 꿀 정보 많으니까 잘 참고하길

--- pages/search.js
+++ pages/search.js
@@ -1,14 +1,28 @@
+import { get } from "http";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function Search() {
+    const [products, setProducts] = useState([]);
     const router = useRouter();
     const { q } = router.query;
+
+    async function getProducts(query) {
+        const res = await axios.get(`/products/?search=${query}`);
+        const nextProducts = res.data.results;
+        setProducts(nextProducts);
+    } 
+
+    useEffect(() => {
+        getProducts(q);
+    },[q]);
 
     return (
         <>
             <h1>Search Page</h1>
             <SearchForm initialValue={q} />
             <h2>{q} 검색 결과</h2>
+            <ProductList products={products} />
         </>
     );
 } 
