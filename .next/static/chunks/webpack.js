904d755afd51f42c6fa9ef0a105f82031457cff9
K0seoyoung <koseo12@hufs.ac.kr>
2025-04-07 05:42:47 +0900

    Image 컴포넌트 사용하기 - 이미지 크기 지정 비율 지정 - 외부 이미지는 미리 이미지 주소 알려주기

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9127a31168ec7f73"; }
+/******/ 		__webpack_require__.h = function() { return "6f8708e05920c2fb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- next.config.js
+++ next.config.js
@@ -10,6 +10,17 @@ const nextConfig = {
       },
     ]
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'learn-codeit-kr-static.s3.ap-northeast-2.amazonaws.com',
+        port: '',
+        pathname: '/codeitmall/**',
+        // search: '',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
